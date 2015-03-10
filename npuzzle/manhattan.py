98b5ef605df262cc97593a420e346a050088f234
Valentin Jarysta <vjarysta@gmail.com>
2015-03-10 19:30:08 +0100

    manhattan: map size implementation

--- npuzzle/manhattan.py
+++ npuzzle/manhattan.py
@@ -7,16 +7,16 @@ current and goal are under the following form :
 '''
 def manhattan(current, goal):
 	distance = 0
-	size = math.sqrt(len(current))
+	size = int(math.sqrt(len(current)))
 	res_x = 0
 	res_y = 0
 	for i in current:
 		pos_difference = abs(goal.index(i) - current.index(i))
 		if i is not 0:
-			res_x = pos_difference % 3
-			res_y = pos_difference / 3
+			res_x = pos_difference % size
+			res_y = pos_difference / size
 			distance += res_x + res_y
-			if (abs(goal.index(i) % 3 - current.index(i) % 3) == 2 and pos_difference % 3 == 1):
+			if (abs(goal.index(i) % size - current.index(i) % size) == size - 1 and pos_difference % size == 1):
 				distance += 2
 	return distance
 
