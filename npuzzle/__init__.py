11179618304a2ca276f88088cc2e0bc0f6afc432
Gildas Saynac <gildas.saynac@gmail.com>
2015-03-10 16:58:10 +0100

    lets get started

---
+++ npuzzle/__init__.py
@@ -0,0 +1,8 @@
+import argparse
+
+def argv_process():
+  parser = argparse.ArgumentParser(description='A Python N-puzzle solver using IDA* algorithm coupled with an optional set of heuristic functions.')
+  args = parser.parse_args()
+
+def main():
+  argv_process()
