28654bbc392706e4df7804dd7735357576160cce
Valentin Jarysta <vjarysta@gmail.com>
2015-03-11 15:30:49 +0100

    misplaced tiles heuristic added

--- npuzzle/manhattan.py
+++ npuzzle/manhattan.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 
 '''
 current and goal are under the following form :
@@ -7,34 +7,15 @@ current and goal are under the following form :
 '''
 def manhattan(current, goal):
 	distance = 0
-	size = int(math.sqrt(len(current)))
+	size = int(sqrt(len(current)))
 	res_x = 0
 	res_y = 0
+	
 	for i in current:
 		if i is not 0:
 			res_x = abs(goal.index(i) % size - current.index(i) % size)
 			res_y = abs(goal.index(i) / size - current.index(i) / size)
 			distance += res_x + res_y
-	return distance
-
-def linear_conflict(current, goal):
-	distance = 0
-	size = int(math.sqrt(len(current)))
-	res_x = 0
-	res_y = 0
-	for i in current:
-		if i is not 0:
-			res_x = abs(goal.index(i) % size - current.index(i) % size)
-			res_y = abs(goal.index(i) / size - current.index(i) / size)
-			distance += res_x + res_y
-			# Conflicts
-			if (current[goal.index(i)] == goal[current.index(i)]):
-				# Horizontal conflict
-				if (goal.index(i) % size - current.index(i) % size >= 1 and goal.index(i) % size - current.index(i) % size < size and goal.index(i) / size - current.index(i) / size == 0):
-					distance += 2
-				# Vertical conflict
-				if (goal.index(i) / size - current.index(i) / size >= 1 and goal.index(i) / size - current.index(i) / size < size and goal.index(i) % size - current.index(i) % size == 0):
-					distance += 2
 	return distance
 
 if (__name__ == '__main__'):
@@ -50,4 +31,3 @@ if (__name__ == '__main__'):
 	]
 
 	print "Manhattan distance:", manhattan(current, goal)
-	print "Linear conflict:", linear_conflict(current, goal)
